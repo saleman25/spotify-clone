a7a7f2b5b2b62d6d74e8d08fd68eb65f029729d1
Samantha Aleman <samantha.aleman25@gmail.com>
2021-07-26 16:09:51 -0700

    finished the logic for logging in and saving the access token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+
+const [token, setToken] = useState(null);
+
+useEffect(() => {
+  const hash = getTokenFromUrl();
+  window.location.hash ='';
+  const _token = hash.access_token;
+  
+  if (_token){
+    setToken(_token)
+  }
+}, []);
+
   return (
     <div className="app">
-      
+      { token ? ( <Player /> ) : ( <Login /> ) }
       <Login />
-      
+
     </div>
   );
 }
