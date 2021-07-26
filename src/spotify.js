4e6849b98efde863e57181cc2aaaaec7bb24a3e6
Samantha Aleman <samantha.aleman25@gmail.com>
2021-07-26 15:40:59 -0700

    adding logic for spotify api call

---
+++ src/spotify.js
@@ -0,0 +1,15 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+const redirectUri = 'http://localhost:3000/';
+
+const clientId = "xa1734f600b194169b4b85dc2780f1c43";
+
+const scopes = [
+    'user-read-currently-playing',
+    'user-read-recently-played',
+    'user-read-playback-state',
+    'user-top-read',
+    'user-modify-playback-state',
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
