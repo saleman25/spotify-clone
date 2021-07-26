a9f1d781e3c27e7b72e950c13fc1e6d1e3c4181a
Samantha Aleman <samantha.aleman25@gmail.com>
2021-07-26 15:15:45 -0700

    starting on login component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Howdy</h1>
+      
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css'
+
+function Login() {
+    return (
+        <div className='login'>
+            {/* Spotify Logo */}
+            {/* Login Button */}
+        </div>
+    )
+}
+
+export default Login
