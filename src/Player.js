085a1c07b1a9f48253a798c8a74b5d92340f1340
Samantha Aleman <samantha.aleman25@gmail.com>
2021-07-27 14:33:12 -0700

    adding in the skeleton for player component

--- src/Player.js
+++ src/Player.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Player.css';
+import Sidebar from './Sidebar';
+import Body from './Body';
 
 function Player({ spotify }) {
     return (
         <div className='player'>
             <div className='player_body'>
-                {/* Sidebar */}
-                {/* Body */}
+                <Sidebar />
+                <Body />
             </div>
             
             {/* Footer */}
-            
+
         </div>
     )
 }
