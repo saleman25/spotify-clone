6bd3c77b037a05f377e6aaad44c236195d02c77a
Samantha Aleman <samantha.aleman25@gmail.com>
2021-07-27 20:20:19 -0700

    started styling on player componenets

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h1>I am foot</h1>
+        </div>
+    )
+}
+
+export default Footer
